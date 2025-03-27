b94549872cae96a859a42c5626f1e6e665a689e3
Ryan Powers <rlp73@georgetown.edu>
2025-03-27 12:27:15 -0400

    Edit WelcomeSection.tsx

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -10,7 +10,9 @@ export default function WelcomeSection({ hasProfile, children }: WelcomeSectionP
     <div className={`${!hasProfile ? 'h-[80vh] flex flex-col justify-center' : ''}`}>
       {!hasProfile && (
         <h1 className="text-center mb-8 text-xl text-gray-700 max-w-2xl mx-auto">
-          Welcome to <span className="font-bold">bskyStats.me</span> — a free tool for analyzing Bluesky posts and engagement for public accounts.
+          Welcome to <span className="font-bold">bskyStats.me</span>
+          <br />
+          A free tool for analyzing Bluesky posts and engagement for public accounts.
         </h1>
       )}
       {children}
